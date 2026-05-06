e4a46a8828e5d990223e28b99a27b73fd5d687f9
avanish_sandilya <bhanusandilya74@gmail.com>
2026-05-06 18:16:27 +0530

    Initial commit

---
+++ BandNameGenerator/main.py
@@ -0,0 +1,12 @@
+print("Welcome to band name generator")
+
+# Taking name of city as input
+city = input("Enter the city you grew up in\n")
+
+# Taking name of car model
+model = input("Enter a car model name\n")
+
+# printing band name
+print("Your band name is:"+" "+city+" "+model)
+
+
