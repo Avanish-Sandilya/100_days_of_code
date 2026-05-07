db6093615fe45e000c055316542d8b611fa799e7
avanish_sandilya <bhanusandilya74@gmail.com>
2026-05-07 19:51:25 +0530

    Adding tipping logic

---
+++ TipCalculator/main.py
@@ -0,0 +1,16 @@
+print("Welcome to tip calculator")
+
+# taking total bill as input
+total = input("Enter the total bill amount\n")
+
+# taking percentage to tip
+tip_percent = input("What percentage would you like to tip\n")
+
+# number of people splitting the bill
+people_count = input("How many people are splitting the bill\n")
+
+# tip calculation
+tip_amount=(float(tip_percent)/100)*float(total)
+tip=tip_amount/float(people_count)
+
+print(f"Each person will pay {round(tip,2)} as tip")
