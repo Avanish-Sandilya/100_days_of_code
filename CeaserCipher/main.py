651321bdfab588d4c97e1a9011a3d7931872684c
avanish_sandilya <bhanusandilya74@gmail.com>
2026-05-11 11:02:11 +0530

    adding decrypt logic

--- CeaserCipher/main.py
+++ CeaserCipher/main.py
@@ -4,17 +4,52 @@ alphabets = [
     'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
-user_input = input("Do you want to encode or decode")
-user_word = input("Enter the text")
-shift_pos = int(input("Enter the shift position"))
+user_input = input("Do you want to encode or decode? ").lower()
+user_word = input("Enter the text: ").lower()
+shift_pos = int(input("Enter the shift position: "))
 
 
 def encrypt(word, shift):
     encrypted_text = ""
 
     for char in word:
-        shifted_position = alphabets.index(char)+shift
+
+        # Ignore spaces
+        if char == " ":
+            encrypted_text += " "
+            continue
+
+        shifted_position = alphabets.index(char) + shift
         shifted_position %= len(alphabets)
+
         encrypted_text += alphabets[shifted_position]
 
-    print(f"The encrypted text is : {encrypted_text}")
+    print(f"The encrypted text is: {encrypted_text}")
+
+
+def decrypt(word, shift):
+    decrypted_text = ""
+
+    for char in word:
+
+        # Ignore spaces
+        if char == " ":
+            decrypted_text += " "
+            continue
+
+        shifted_position = alphabets.index(char) - shift
+        shifted_position %= len(alphabets)
+
+        decrypted_text += alphabets[shifted_position]
+
+    print(f"The decrypted text is: {decrypted_text}")
+
+
+if user_input == "encode":
+    encrypt(user_word, shift_pos)
+
+elif user_input == "decode":
+    decrypt(user_word, shift_pos)
+
+else:
+    print("Invalid choice")
