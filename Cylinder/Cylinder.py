c536793f5fde5558c07b2ba83c04456bbb21118b
avanish_sandilya <bhanusandilya74@gmail.com>
2026-05-14 13:10:56 +0530

    adding volume and SA formulas

---
+++ Cylinder/Cylinder.py
@@ -0,0 +1,16 @@
+class Cylinder:
+
+    pi = 3.14
+
+    def __init__(self, radius=1, height=1):
+        self.radius = radius
+        self.height = height
+
+    def volume(self):
+        return self.pi * (self.radius**2) * self.height
+
+    def surface_area(self):
+        return (2*self.pi*(self.radius**2)) + (2*self.pi*self.radius*self.height)
+
+
+
---
+++ Cylinder/main.py
@@ -0,0 +1,6 @@
+from Cylinder import Cylinder
+
+c = Cylinder(3, 2)
+
+print(c.volume())
+print(c.surface_area())
