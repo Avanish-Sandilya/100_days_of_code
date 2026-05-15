205378991befe1409988ad44db2b6186e982f5ac
avanish_sandilya <bhanusandilya74@gmail.com>
2026-05-15 14:50:30 +0530

    adding dashed line

--- Turtle/main.py
+++ Turtle/main.py
@@ -4,10 +4,17 @@ timmy = Turtle()
 timmy.shape("turtle")
 
 # Making a square
-for _ in range(4):
-    timmy.forward(100)
-    timmy.right(90)
+# for _ in range(4):
+#     timmy.forward(100)
+#     timmy.right(90)
 
+
+# Drawing dashed line
+# for _ in range(50):
+#     timmy.forward(10)
+#     timmy.penup()
+#     timmy.forward(10)
+#     timmy.pendown()
 
 screen = Screen()
 screen.exitonclick()
