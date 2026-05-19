51ea1914bc2d45819914b559867c4ba2265bdbfc
avanish_sandilya <bhanusandilya74@gmail.com>
2026-05-19 14:32:04 +0530

    turtle shapes

--- Turtle/main.py
+++ Turtle/main.py
@@ -16,5 +16,16 @@ timmy.shape("turtle")
 #     timmy.forward(10)
 #     timmy.pendown()
 
+def draw_shape(num_sides):
+    angle = 360/num_sides
+    for _ in range(num_sides):
+        timmy.forward(100)
+        timmy.right(angle)
+
+
+for shape_side_n in range(3,11):
+    draw_shape(shape_side_n)
+
+
 screen = Screen()
 screen.exitonclick()
